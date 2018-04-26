c3fc8e8b336f481ab3dc8cd6bdce037af40b45c3
heartjhl <2858796570@qq.com>
2018-04-26 11:05:16 +0800

    创建工程

---
+++ 20180426/20180426/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20180426
+//
+//  Created by heartjhl on 2018/4/26.
+//  Copyright © 2018年 heartjhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20180426/20180426/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  20180426
+//
+//  Created by heartjhl on 2018/4/26.
+//  Copyright © 2018年 heartjhl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"世界上最难🉐️得是莫过于珍惜彼此");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
